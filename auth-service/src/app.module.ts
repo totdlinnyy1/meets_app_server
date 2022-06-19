f7bc1777f1ffafbaa0c3bcedee78336f1280b1fb
tot_dlinnyy <gnngr333@gmail.com>
2022-06-19 10:53:25 +0700

    feat: added typeorm

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+import { config } from './config/config'
+import { typeOrmConfig } from './config/typeorm.config'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot(config),
+        TypeOrmModule.forRootAsync(typeOrmConfig),
+    ],
 })
 export class AppModule {}
--- rooms-service/src/main.ts
+++ rooms-service/src/main.ts
@@ -10,15 +10,15 @@ const DEFAULT_APP_HORT = 'localhost'
 const DEFAULT_APP_PORT = 3000
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule)
+    const app = await NestFactory.create(AppModule)
 
-  const configService = app.get(ConfigService)
+    const configService = app.get(ConfigService)
 
-  const port = configService.get('PORT') || DEFAULT_APP_PORT
-  const hostname = configService.get('HOST') || DEFAULT_APP_HORT
+    const port = configService.get('PORT') || DEFAULT_APP_PORT
+    const hostname = configService.get('HOST') || DEFAULT_APP_HORT
 
-  await app.listen(port, hostname, () =>
-    logger.log(`Server running at ${hostname}:${port}`),
-  )
+    await app.listen(port, hostname, () =>
+        logger.log(`Server running at ${hostname}:${port}`),
+    )
 }
 bootstrap()
