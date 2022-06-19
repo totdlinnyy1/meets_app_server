de318afee72f61abf88fae09e99b33098f6ace7d
tot_dlinnyy <gnngr333@gmail.com>
2022-06-19 10:37:59 +0700

    feat: set up the environment, changed the main file, removed unnecessary

--- rooms-service/src/app.module.ts
+++ rooms-service/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {ConfigModule} from '@nestjs/config'
+import {config} from './config/config'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(config),
+  ],
 })
 export class AppModule {}
